0298695f6a619948dc132356c2209a06acb63651
Vijay Digambar Kusekar <vijay.kusekar2003@gmail.com>
2024-06-06 16:02:37 +0530

    Update layout.js

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -4,7 +4,7 @@ import Sidenav from './_components/Sidenav';
 import TopHeader from './_components/TopHeader';
 
 function layout({children}) {
-  const [toggle , setToggle] = useState(false);
+  // const [toggle , setToggle] = useState(false);
   return (
     <div>
       <div
@@ -12,14 +12,14 @@ function layout({children}) {
       fixed inset-y-0 z-30 
       md:flex bg-white"
       >
-        <Sidenav closeSideBar={() => setToggle(false)} />
+        <Sidenav />
       </div>
 
-      {toggle ? (
-        <div className=" h-full w-64 flex-col fixed inset-y-0 z-30 bg-white flex">
-          <Sidenav closeSideBar={() => setToggle(false)} />
-        </div>
-      ) : null}
+      // {toggle ? (
+      //   <div className=" h-full w-64 flex-col fixed inset-y-0 z-30 bg-white flex">
+      //     <Sidenav closeSideBar={() => setToggle(false)} />
+      //   </div>
+      // ) : null}
 
       <div className=" md:ml-64">
         <TopHeader />
